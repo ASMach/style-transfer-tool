2692b37b7568983da14b1eb79d196d876175d9d8
Alonzo Machiraju <machthree@gmail.com>
2024-02-17 23:28:17 -0800

    update database settings for production
    postgresql to avoid migration issues down the line

--- image_transer_deployment/image_transer_deployment/settings.py
+++ image_transer_deployment/image_transer_deployment/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'image_transer_deployment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "TEST": {
+            "NAME": "mytestdatabase",
+        },
     }
 }
 
