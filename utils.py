181104c52a09537623d0a7f51bb6eb843d8837d1
Alonzo Machiraju <machthree@gmail.com>
2024-02-25 18:16:49 -0800

    Define models

--- utils.py
+++ utils.py
@@ -76,3 +76,44 @@ def check_device():
         device = torch.device("cuda" if (torch.cude.is_available()) else 'cpu')
 
     return device
+
+# initially it takes some time for PyTorch to download the models into local cache
+
+
+def prepare_model(model, device):
+    # we are not tuning model weights -> we are only tuning optimizing_img's pixels! (that's why requires_grad=False)
+    experimental = False
+    if model == 'vgg16':
+        if experimental:
+            # much more flexible for experimenting with different style representations
+            model = Vgg16Experimental(requires_grad=False, show_progress=True)
+        else:
+            model = Vgg16(requires_grad=False, show_progress=True)
+    elif model == 'vgg19':
+        model = Vgg19(requires_grad=False, show_progress=True)
+    else:
+        raise ValueError(f'{model} not supported.')
+
+    content_feature_maps_index = model.content_feature_maps_index
+    style_feature_maps_indices = model.style_feature_maps_indices
+    layer_names = model.layer_names
+
+    content_fms_index_name = (
+        content_feature_maps_index, layer_names[content_feature_maps_index])
+    style_fms_indices_names = (style_feature_maps_indices, layer_names)
+    return model.to(device).eval(), content_fms_index_name, style_fms_indices_names
+
+
+def gram_matrix(x, should_normalize=True):
+    (b, ch, h, w) = x.size()
+    features = x.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t)
+    if should_normalize:
+        gram /= ch * h * w
+    return gram
+
+
+def total_variation(y):
+    return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + \
+        torch.sum(torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :]))
