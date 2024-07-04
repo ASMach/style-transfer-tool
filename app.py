7949d84ff8bc40ad0cd39d43ad98d716b0aa24b0
Alonzo Machiraju <machthree@gmail.com>
2024-07-03 20:56:18 -0700

    Fix upload flow

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, request, redirect, flash, url_for, render_template, sen
 from werkzeug.utils import secure_filename
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from pathlib import Path
 
 import os
 import sys
 
 from train_transfer_image_style import train_transfer_image_style
 
-UPLOAD_FOLDER = '/uploads'
-OUTPUT_FOLDER = '/output'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+
+print(ROOT_DIR, file=sys.stderr)
+
+UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'output'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024  # Max file 512MB
@@ -32,6 +38,9 @@ def home():
 
 @app.route('/uploads', methods=['GET'])
 def uploads():
+    # Verify the output folder exists and create one if it doesn't
+    Path(app.config['OUTPUT_FOLDER']).mkdir(parents=True, exist_ok=True)
+
     files = os.listdir(app.config['OUTPUT_FOLDER'])
     return render_template('uploads.html', files=files)
 
@@ -46,8 +55,8 @@ def submit(epochs, width, height):
 @app.route('/transfer_style/', methods=['POST'])
 def transfer_style():
     epochs = request.form['epochSlider']
-    width = request.form['widthSlider']
-    height = request.form['heightSlider']
+    width = int(request.form['widthSlider'])
+    height = int(request.form['heightSlider'])
 
     print("Epochs: " + epochs, file=sys.stderr)
     print(f"{width} x {height}", file=sys.stderr)
@@ -73,23 +82,30 @@ def transfer_style():
         if target.filename == '':
             flash('No selected target image')
             return redirect(request.url)
+        
+        # Create the upload folder if it does not exist
+        Path(app.config['UPLOAD_FOLDER']).mkdir(parents=True, exist_ok=True)
 
         if source and target and allowed_file(source.filename) and allowed_file(target.filename):
-            f = secure_filename(source)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], source))
-            f = secure_filename(target)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], target))
-            return redirect(url_for('download_file'))
+            source_filename = secure_filename(source.filename)
+            source.save(os.path.join(app.config['UPLOAD_FOLDER'], source_filename))
+            target_filename = secure_filename(target.filename)
+            target.save(os.path.join(app.config['UPLOAD_FOLDER'], target_filename))
 
-        train_transfer_image_style(
-            source,
-            target,
-            os.path.join(app.config['OUTPUT_FOLDER'],
-                         f'{source}-{target}.png'),
-            epochs,
-            'vgg19',
-            width,
-            height)
+            # Verify the output folder exists and create one if it doesn't
+            Path(app.config['OUTPUT_FOLDER']).mkdir(parents=True, exist_ok=True)
+
+            # Actually generate the style transfer image
+            train_transfer_image_style(
+                source,
+                target,
+                os.path.join(app.config['OUTPUT_FOLDER'],
+                         f'{source.filename}-{target.filename}.png'),
+                epochs,
+                'vgg19',
+                width,
+                height)
+        return redirect(url_for('download_file'))
     return redirect(url_for("uploads"))
 
 
